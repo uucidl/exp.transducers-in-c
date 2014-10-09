d5e1a8a831d2a3027fc89712d13bf67d86cc6412
Nicolas Léveillé <nicolas@uucidl.com>
2014-10-09 09:39:00 +0200

    implement reduce over streams
    streams are the basic step function of our transducer. optimized
    for arrays, but could work for any datastructure that can be
    linearized.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "allocator.h"
 #include "allocator_type.h"
+#include "stream_types.h"
 
 #include <assert.h>
 #include <stdio.h>
@@ -13,14 +14,77 @@ enum TypeTags
 struct Value
 {
         int type_tag;
-        void* address;
+        size_t element_size;
+        void const* address;
 };
+
+struct Reducer;
+
+// reducer closure
+struct Reducer
+{
+        struct Value (*zero)(struct Reducer *reducer);
+        struct Value (*apply)(struct Reducer *reducer, struct Value input, struct Value current);
+        struct Allocator* allocator;
+};
+
+struct ValueStreamRange
+{
+        int type_tag;
+        size_t element_size;
+        uint8_t const *start;
+        uint8_t const *end;
+        uint8_t const *cursor;
+        enum StreamErrorCode error;
+        enum StreamErrorCode (*next)(struct ValueStreamRange*);
+};
+
+static enum StreamErrorCode zerosVSRNext(struct ValueStreamRange* range)
+{
+        static uint8_t const zeros[256] = {0};
+
+        range->type_tag = 0;
+        range->element_size = 1;
+        range->start = zeros;
+        range->cursor = zeros;
+        range->end = zeros + sizeof(zeros);
+
+        return range->error;
+}
+
+static enum StreamErrorCode failVSR(struct ValueStreamRange* range, enum StreamErrorCode error)
+{
+        range->error = error;
+        range->next = zerosVSRNext;
+        return range->next(range);
+}
+
+static enum StreamErrorCode floatArrayNext(struct ValueStreamRange* range)
+{
+        return failVSR(range, S_ReadPastEnd);
+}
+
+static
+void floatArrayVSR(struct ValueStreamRange* range, float const* values, size_t count)
+{
+        range->type_tag = TTAG_FLOAT;
+        range->element_size = sizeof(float);
+        range->start = (uint8_t*) values;
+        range->cursor = (uint8_t*) values;
+        range->end = (uint8_t*) (values + count);
+        range->error = S_NoError;
+        range->next = floatArrayNext;
+}
 
 static struct Value floatValue(float const f, struct Allocator * const allocator)
 {
         float* result = allocator_alloc(allocator, sizeof *result);
         *result = f;
-        return (struct Value) { .type_tag = TTAG_FLOAT, .address = result };
+        return (struct Value) {
+                .type_tag = TTAG_FLOAT,
+                .element_size = sizeof *result,
+                .address = result
+        };
 }
 
 /* reducer example */
@@ -34,13 +98,60 @@ struct Value accumulateFloat(
         assert (input.type_tag == TTAG_FLOAT);
         assert (current.type_tag == TTAG_FLOAT);
 
-        float* inputFloat = input.address;
-        float* currentFloat = current.address;
+        float const* inputFloat = input.address;
+        float const* currentFloat = current.address;
 
         return floatValue(*inputFloat + *currentFloat, allocator);
 }
 
-typedef struct Value (*ReducerFn)(struct Value input, struct Value current, struct Allocator * allocator);
+static
+struct Value accumulateFloatZero(struct Reducer* reducer)
+{
+        static float zero = 0.0f;
+        return (struct Value) {
+                .type_tag = TTAG_FLOAT,
+                .element_size = sizeof zero,
+                .address = &zero,
+        };
+}
+
+static
+struct Value accumulateFloatApply(struct Reducer* reducer, struct Value const input, struct Value const current)
+{
+        return accumulateFloat(input, current, reducer->allocator);
+}
+
+static
+struct Value nextValueVSR(struct ValueStreamRange* range)
+{
+        static struct Value zero;
+
+        while (range->error == S_NoError) {
+                if (range->cursor < range->end) {
+                        void const* valueAddress = range->cursor;
+                        range->cursor += range->element_size;
+                        return (struct Value) {
+                                .type_tag = range->type_tag,
+                                        .element_size = range->element_size,
+                                        .address = valueAddress,
+                                        };
+                }
+                range->next(range);
+        }
+
+        return zero;
+}
+
+static
+struct Value reduce(struct ValueStreamRange* range, struct Reducer* reducer, struct Allocator * allocator)
+{
+        struct Value element;
+        struct Value result = reducer->zero(reducer);
+        while ((element = nextValueVSR(range), range->error == S_NoError)) {
+                result = reducer->apply(reducer, element, result);
+        }
+        return result;
+}
 
 static
 void* stdlib_alloc(struct Allocator * const allocator, size_t size)
@@ -61,9 +172,27 @@ int main (int argc, char** argv)
                 .free = stdlib_free,
         };
 
-        struct Value result = accumulateFloat(floatValue(1.0f, &heapAllocator), floatValue(3.0f, &heapAllocator), &heapAllocator);
+        printf("1. individual test\n");
+        {
+                struct Value result = accumulateFloat(floatValue(1.0f, &heapAllocator), floatValue(3.0f, &heapAllocator), &heapAllocator);
 
-        printf("result is: %f", *((float*)result.address));
+                printf("result is: %f\n", *((float*)result.address));
+        }
+
+        printf("2. process array as stream\n");
+        {
+                float values[] = { 1.0, 2.0, 3.0, 4.0 };
+                struct ValueStreamRange valuesRange;
+                struct Reducer accumulator = {
+                        .zero = accumulateFloatZero,
+                        .apply = accumulateFloatApply,
+                        .allocator = &heapAllocator,
+                };
+                floatArrayVSR(&valuesRange, values, sizeof values / sizeof values[0]);
+
+                struct Value result = reduce(&valuesRange, &accumulator, &heapAllocator);
+                printf("result is: %f\n", *((float*) result.address));
+        }
 
         return 0;
 }
